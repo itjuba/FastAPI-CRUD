90dcd5d5273725b284a897359621e873ce96e216
itjuba <attignadjib@outlook.com>
2021-09-11 12:28:43 +0100

    fixes

--- accounts/models.py
+++ accounts/models.py
@@ -15,6 +15,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    is_active = Column(Boolean, default=False)
 
 
